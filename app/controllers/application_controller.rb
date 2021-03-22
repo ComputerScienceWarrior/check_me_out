9d6592f3e55e1311058cd5741796f9e1a81a407d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-22 14:11:20 -0700

    Move DeviseWhitelist functionality from application_controller.rb to a concerns file devise_whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-    # only run the before filter if we receive a devise_controller...(special devise helper method)
-    before_action :configure_permitted_params, if: :devise_controller?
-
-    def configure_permitted_params
-        # Additional params to sanitize and allow during signup and edit
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+    include DeviseWhitelist
 end
