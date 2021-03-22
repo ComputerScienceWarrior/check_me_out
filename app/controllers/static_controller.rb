a6a9fb45199da949ae9c9afc09e459e70ad49f63
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-22 12:22:25 -0700

    create home action with @greeting.Display in home view.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,6 @@
 class StaticController < ApplicationController
+    def home
+        #add a feature here to abstract special greeting based on day to a helper method.
+        @greeting = "Welcome to the Home Page!"
+    end
 end
