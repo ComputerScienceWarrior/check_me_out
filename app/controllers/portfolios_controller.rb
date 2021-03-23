c3190b02d6117fe154342433b83102a5548194b2
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-22 21:21:26 -0700

    Create index show new and create actions.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,24 @@
 class PortfoliosController < ApplicationController
+    # access is allowing access to specific petergate defined roles from the petergate gem
+    access all: [:show, :index], user: {except: [:destroy]}, admin: :all
+
+    def index
+        @portfolios = Portfolio.all
+    end
+
+    def show
+    end
+
+    def new
+        @portfolio = Portfolio.new
+    end
+
+    def create
+        @portfolio = Portfolio.new(portfolio_params)
+        if @portfolio.save
+            # logic to redirect ot nwe portfolio's show page
+        else
+            render :new
+        end
+    end
 end
