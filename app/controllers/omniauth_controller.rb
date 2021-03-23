9bd8ce9d54c75f95c8128c876163b1bbb4c9799f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-23 12:37:41 -0700

    create omniauth controller for various providers.

---
+++ app/controllers/omniauth_controller.rb
@@ -0,0 +1,34 @@
+class OmniauthController < ApplicationController
+
+    def facebook
+        @user = User.create_from_provider_data(request.env["omniauth"])
+        if @user.save
+            sign_in_and_redirect @user
+        else
+            redirect_to new_user_registration_url
+        end
+    end
+
+    def github
+        @user = User.create_from_github_data(request.env["omniauth"])
+        if @user.save
+            sign_in_and_redirect @user
+        else
+            redirect_to new_user_registration_url
+        end
+    end
+
+    def google_oauth2
+        @user = User.create_from_google_data(request.env["omniauth"])
+        if @user.save
+            sign_in_and_redirect @user
+        else
+            redirect_to new_user_registration_url
+        end
+    end
+    
+    def failure
+        flash[:error] = "There was an error signing into the page, please try again later."
+        redirect_to new_user_registration_url
+    end
+end
