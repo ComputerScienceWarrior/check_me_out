5177381d40fd0386695c766305a6f4feee5a36ee
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-23 12:42:17 -0700

    Add self.create_from_provider_data class method to create user via omniauth data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2]
 
   validates_presence_of :first_name, :last_name
   
   # virtual attribute full_name
   def full_name
     self.first_name + " " + self.last_name
+  end
+
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20] # password not provided by the providers, so we create our own
+    end
   end
 end
