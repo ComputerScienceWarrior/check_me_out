a3cfbe859f6d7444ccd25ea0d984f57e94b4c0b2
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-24 00:43:40 -0700

    run migration for createportfolios

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_191234) do
+ActiveRecord::Schema.define(version: 2021_03_24_074209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "name"
+    t.text "header"
+    t.text "description"
+    t.string "category"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
