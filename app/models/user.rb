40b4940e858a611cd5ca15462d9e21c04e74e6d0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-25 10:03:43 -0700

    remove conditional block in .create_from_provider_data class method. Update username definition within self.create_from_provider_data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2]
@@ -14,14 +14,10 @@ class User < ApplicationRecord
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
-      if provider_data.info.first_name.nil? || provider_data.info.last_name.nil?
-        user.first_name, user.last_name = provider_data.info.name.split 
-      else
-        user.first_name = provider_data.info.first_name
-        user.last_name = provider_data.info.last_name
-      end
+      user.first_name, user.last_name = provider_data.info.name.split
+      user.username = self.name.split.join("-") + rand(1000).to_i.to_s
       user.email = provider_data.info.email
-      user.password = Devise.friendly_token[0, 20] # password not provided by the providers, so we create our own
+      user.password = Devise.friendly_token[8, 20] # password not provided by the providers, so we create our own
     end
   end
 end
