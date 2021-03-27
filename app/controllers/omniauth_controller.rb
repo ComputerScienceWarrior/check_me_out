81e19c17cd675c4e62052ef3ad08506de7b5e8fa
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-26 20:32:09 -0700

    comment out facebook omniauth action. add session in github sign in/up

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,18 +1,20 @@
 class OmniauthController < Devise::OmniauthCallbacksController
 
-    def facebook
-        @user = User.create_from_provider_data(request.env["omniauth.auth"])
-        if @user.save
-            redirect_to user_path(@user)
-        else
-            flash[:error] = "An error occured!"
-            redirect_to new_user_registration_url
-        end
-    end
+    # future facebook implementation below
+    # def facebook
+    #     @user = User.create_from_provider_data(request.env["omniauth.auth"])
+    #     if @user.save
+    #         redirect_to user_path(@user)
+    #     else
+    #         flash[:error] = "An error occured!"
+    #         redirect_to new_user_registration_url
+    #     end
+    # end
 
     def github
         @user = User.create_from_provider_data(request.env["omniauth.auth"])
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             redirect_to new_user_registration_url
