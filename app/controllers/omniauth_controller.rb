e8bde9a2e2eeb2b1edcff3fd867e29ccb8361ba0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-28 21:19:56 -0700

    Add Devise helpers #sign_in and remove session[:user_id] calls

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -13,8 +13,8 @@ class OmniauthController < Devise::OmniauthCallbacksController
 
     def github
         @user = User.create_from_provider_data(request.env["omniauth.auth"])
+        sign_in(:user, @user)
         if @user.save
-            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             redirect_to new_user_registration_url
@@ -23,8 +23,8 @@ class OmniauthController < Devise::OmniauthCallbacksController
 
     def google_oauth2
         @user = User.create_from_provider_data(request.env["omniauth.auth"])
+        sign_in(:user, @user)
         if @user.save
-            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             redirect_to new_user_registration_url
